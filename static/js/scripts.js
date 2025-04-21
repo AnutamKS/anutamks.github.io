bb4521e3508571dad7b619367e187f036ba25bba
Anutam Kandadai Srinivasan <srinivasan.268@buckeyemail.osu.edu>
2025-04-20 20:36:06 -0400

    correction

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'work', 'awards']
+const section_names = ['home', 'publications', 'work']
 
 
 window.addEventListener('DOMContentLoaded', event => {
